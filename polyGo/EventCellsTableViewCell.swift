4aa3e08b770221f9d711c3706bde4366346bae1e
Artwaitforit <liraarturo13@yahoo.com>
2017-11-30 12:41:14 -0800

    60% with thicc borders

---
+++ polyGo/EventCellsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventCellsTableViewCell.swift
+//  
+//
+//  Created by Arturo Lira on 11/27/17.
+//
+
+import UIKit
+
+class EventCellsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
